feeb8506cba210fcdc82cd198073a25cb75a6c54
Alexis Jean Ríos Haro <al.riosh@duocuc.cl>
2025-05-10 03:13:09 -0400

    Elaboracion de controlador cliente

--- src/main/java/com/duoc/EcoMarket/controller/ClienteController.java
+++ src/main/java/com/duoc/EcoMarket/controller/ClienteController.java
@@ -1,7 +1,23 @@
 package com.duoc.EcoMarket.controller;
 
+import com.duoc.EcoMarket.model.Cliente;
+import com.duoc.EcoMarket.services.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
-
+@RestController
+@RequestMapping("api/v1/clientes")
 public class ClienteController {
+
+    @Autowired
+    private ClienteService ClienteService;
+
+    @GetMapping
+    public List<Cliente> listarClientes(){
+        return ClienteService.getClientes()
+    }
+
+
 }
